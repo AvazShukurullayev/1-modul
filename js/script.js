0f9a6adf9eafd72591d86067b56ec7f0b93525c6
Avaz <elonarm21@gmail.com>
2024-02-29 00:50:54 +0500

    added files

--- js/script.js
+++ js/script.js
@@ -1,66 +1,59 @@
 "use strict"
 
-let numberOfSeries
-
-function startApp() {
-    do {
-        numberOfSeries = +prompt("Nechta serial ko'rdingiz?", "")
-    } while (numberOfSeries === null || numberOfSeries === "" || numberOfSeries === 0 || isNaN(numberOfSeries))
-}
-
-startApp()
 const seriesDb = {
-    count: numberOfSeries,
+    count: 0,
     series: {},
     actors: {},
     genres: [],
-    private: false
-}
-
-function favouriteSeries() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("Oxirgi ko'rgan serialingiz?", ""),
-            b = prompt("Necha baxo berasiz?", "");
-        if (a !== null && b !== null && a !== "" && b !== "") {
-            seriesDb.series[a] = b
-            console.log("Done")
-        } else {
-            i--
+    private: false,
+    start: function () {
+        do {
+            this.count = +prompt("Nechta serial ko'rdingiz?", "")
+        } while (this.count === null || this.count === "" || this.count === 0 || isNaN(this.count))
+    },
+    favouriteSeries: function () {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("Oxirgi ko'rgan serialingiz?", ""),
+                b = prompt("Necha baxo berasiz?", "");
+            if (a !== null && b !== null && a !== "" && b !== "") {
+                this.series[a] = b
+                console.log("Done")
+            } else {
+                i--
+            }
         }
-    }
-}
-
-favouriteSeries()
-
-function showDb() {
-    if (!seriesDb.private) {
-        console.log(seriesDb)
-    }
-}
-
-showDb()
-
-function writeGenres() {
-    for (let i = 0; i < 3; i++) {
-        const userFavouriteSeries = prompt(`yaxshi ko'rgan janringiz ${i + 1}?`)
-        seriesDb.genres.push(userFavouriteSeries)
-    }
-}
-
-writeGenres()
-
-function detectingSeriesCount(count) {
-    if (count !== null && count !== 0) {
-        if (1 <= count && count < 5) {
-            console.log(`Kam serial korarkansiz`)
-        } else if (5 <= count && count <= 10) {
-            console.log(`Siz classic tomoshabin ekansiz`)
-        } else if (count > 10) {
-            console.log(`Siz serialchi bolsangiz kere`)
+    },
+    showDb: function () {
+        if (!seriesDb.private) {
+            console.log(seriesDb)
         } else {
-            console.log("Qandaydur xatolik yuz berdi!")
+            console.log("Something went wrong!")
         }
-    }
+    },
+    writeGenres: function () {
+        for (let i = 0; i < 3; i++) {
+            const newGenre = prompt(`yaxshi ko'rgan janringiz ${i + 1}?`, "")
+            if (newGenre !== null && newGenre !== "" && newGenre.trim()) {
+                this.genres[i] = newGenre
+            } else {
+                i--
+            }
+        }
+    },
+    detectingSeriesCount: function () {
+        if (this.count !== null && this.count !== 0) {
+            if (1 <= this.count && this.count < 5) {
+                console.log(`Kam serial korarkansiz`)
+            } else if (5 <= this.count && this.count <= 10) {
+                console.log(`Siz classic tomoshabin ekansiz`)
+            } else if (this.count > 10) {
+                console.log(`Siz serialchi bolsangiz kere`)
+            } else {
+                console.log("Qandaydur xatolik yuz berdi!")
+            }
+        }
+    },
+    visibleDb: function () {
+        this.private = !this.private;
+    },
 }
-
-detectingSeriesCount(seriesDb.count)
